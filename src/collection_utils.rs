6c105702c8b5819a5cfe70208ee26089cde643d4
Taylor Allred <taylor.allred21@gmail.com>
2022-08-12 13:37:09 -0600

    made List and Sequence to make immutable recursion better

--- src/collection_utils.rs
+++ src/collection_utils.rs
@@ -1,4 +1,5 @@
 use im_rc::vector::*;
+use std::str::Chars;
 
 pub trait Collection<T: Clone> {
     fn conj(self, value: T) -> Self;
@@ -15,5 +16,51 @@ impl Collection<char> for String {
     fn conj(mut self, value: char) -> Self {
         self.push(value);
         self
-    } 
-} 
+    }
+}
+
+#[derive(Clone)]
+pub enum List<T: Clone> {
+    Empty,
+    Cons(T, Box<List<T>>),
+}
+
+impl<T: Clone> List<T> {
+    pub fn empty() -> Self {
+        List::Empty
+    }
+
+    pub fn cons(self, value: T) -> Self {
+        List::Cons(value, Box::new(self))
+    }
+
+    pub fn head(self) -> Option<T> {
+        match self {
+            List::Empty => None,
+            List::Cons(value, _) => Some(value),
+        }
+    }
+
+    pub fn tail(self) -> Self {
+        match self {
+            List::Empty => List::Empty,
+            List::Cons(_, rest) => *rest,
+        }
+    }
+}
+
+pub trait Sequence: Iterator + DoubleEndedIterator {
+    fn seq(self) -> List<Self::Item>
+    where
+        Self::Item: Clone,
+        Self: Sized,
+    {
+        let mut lst = List::Empty;
+        for item in self.rev() {
+            lst = lst.cons(item);
+        }
+        lst
+    }
+}
+
+impl<'a> Sequence for Chars<'a> {}
--- src/tokenizer.rs
+++ src/tokenizer.rs
@@ -1,6 +1,4 @@
 #![allow(dead_code)]
-use std::iter::Peekable;
-use std::str::Chars;
 
 use crate::collection_utils::*;
 use im_rc::vector;
@@ -26,54 +24,60 @@ pub struct Token {
 
 impl Token {
     fn new(token_type: TokenType, lexeme: String) -> Self {
-        Self {
-            token_type,
-            lexeme,
-        }
+        Self { token_type, lexeme }
     }
 }
 
 pub fn tokenize(program: String) -> Vector<Token> {
     #[tailcall]
-    fn tokenize_inner(mut p: Peekable<Chars>, tokens: Vector<Token>) -> Vector<Token> {
-        match p.next() {
+    fn tokenize_inner(p: List<char>, tokens: Vector<Token>) -> Vector<Token> {
+        match p.clone().head() {
             None => tokens,
             Some(curr) => match curr {
                 '[' => tokenize_inner(
-                    p,
+                    p.tail(),
                     tokens.conj(Token::new(TokenType::LeftBracket, String::from(curr))),
                 ),
                 ']' => tokenize_inner(
-                    p,
+                    p.tail(),
                     tokens.conj(Token::new(TokenType::RightBracket, String::from(curr))),
                 ),
                 '.' => tokenize_inner(
-                    p,
+                    p.tail(),
                     tokens.conj(Token::new(TokenType::Dot, String::from(curr))),
                 ),
                 ',' => tokenize_inner(
-                    p,
+                    p.tail(),
                     tokens.conj(Token::new(TokenType::Comma, String::from(curr))),
                 ),
-                c if c.is_alphabetic() => tokenize_identifier(p, tokens, String::from(curr)),
-                c if c.is_ascii_digit() => tokenize_number(p, tokens, String::from(curr), false),
-                _ => tokenize_inner(p, tokens),
+                '<' => match p.clone().tail().head() {
+                    None => tokens,
+                    Some('-') => tokenize_inner(
+                        p.tail(),
+                        tokens.conj(Token::new(TokenType::LeftArrow, String::from("<-"))),
+                    ),
+                    Some(_) => tokenize_inner(p.tail(), tokens),
+                },
+                c if c.is_alphabetic() => tokenize_identifier(p.tail(), tokens, String::from(curr)),
+                c if c.is_ascii_digit() => {
+                    tokenize_number(p.tail(), tokens, String::from(curr), false)
+                }
+                _ => tokenize_inner(p.tail(), tokens),
             },
         }
     }
 
     #[tailcall]
     fn tokenize_identifier(
-        mut p: Peekable<Chars>,
+        p: List<char>,
         tokens: Vector<Token>,
         accumulated: String,
     ) -> Vector<Token> {
-        match p.peek() {
+        match p.clone().head() {
             None => tokens.conj(Token::new(TokenType::Identifier, accumulated)),
             Some(nxt) => match nxt {
                 c if c.is_alphabetic() => {
-                    let accumulated = accumulated.conj(p.next().unwrap());
-                    tokenize_identifier(p, tokens, accumulated)
+                    tokenize_identifier(p.tail(), tokens, accumulated.conj(nxt))
                 }
                 _ => tokenize_inner(
                     p,
@@ -85,26 +89,24 @@ pub fn tokenize(program: String) -> Vector<Token> {
 
     #[tailcall]
     fn tokenize_number(
-        mut p: Peekable<Chars>,
+        p: List<char>,
         tokens: Vector<Token>,
         accumulated: String,
         dot_appeard: bool,
     ) -> Vector<Token> {
-        match p.peek() {
+        match p.clone().head() {
             None => tokens.conj(Token::new(TokenType::Number, accumulated)),
             Some(nxt) => match nxt {
                 c if c.is_ascii_digit() => {
-                    let accumulated = accumulated.conj(p.next().unwrap());
-                    tokenize_number(p, tokens, accumulated, dot_appeard)
+                    tokenize_number(p.tail(), tokens, accumulated.conj(nxt), dot_appeard)
                 }
                 '.' if !dot_appeard => {
-                    let accumulated = accumulated.conj(p.next().unwrap());
-                    tokenize_number(p, tokens, accumulated, true)
+                    tokenize_number(p.tail(), tokens, accumulated.conj(nxt), true)
                 }
                 _ => tokenize_inner(p, tokens.conj(Token::new(TokenType::Number, accumulated))),
             },
         }
     }
 
-    tokenize_inner(program.chars().peekable(), vector![])
+    tokenize_inner(program.chars().seq(), vector![])
 }
